59d680ad436189044ff40cebeab29597bff83303
Sagnikkundu3397 <kundusagnik988@gmail.com>
2026-01-16 01:00:15 +0530

    create flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,render_template
+from Flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
 
@@ -13,6 +13,7 @@ app=application
 
 @app.route('/')
 def index():
+    
     return render_template('index.html') 
 
 @app.route('/predictdata',methods=['GET','POST'])
@@ -26,9 +27,8 @@ def predict_datapoint():
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
-
+            reading_score=float(request.form.get('reading_score')),
+            writing_score=float(request.form.get('writing_score'))  
         )
         pred_df=data.get_data_as_data_frame()
         print(pred_df)
